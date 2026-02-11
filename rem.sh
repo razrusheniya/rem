db5f85a15b81cec793b113328507d16959f11dd3
らずるしぇーにゃ <razrusheniya@outlook.com>
2026-02-11 09:44:10 +0900

    Update rem.sh

--- rem.sh
+++ rem.sh
@@ -1,6 +1,20 @@
-cat $1 | rem > main.asm
-shift
+run() {
+    cat $1 | rem > main.asm
+    shift
 
-nasm -f elf64 -g -F dwarf -o main.o main.asm
-gcc -no-pie -O3 -rdynamic -o main main.o $(pkg-config --cflags --libs gtk+-3.0)
-./main "$@"
+    nasm -f elf64 -g -F dwarf -o main.o main.asm
+    gcc -no-pie -O3 -rdynamic -o main main.o $(pkg-config --cflags --libs gtk+-3.0)
+    ./main "$@"
+}
+
+install() {
+    apt install nasm gcc libgtk-3-dev pkg-config curl
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    . "$HOME/.cargo/env"
+    cargo install --path .
+}
+
+update() {
+    git pull
+    cargo install --path .
+}

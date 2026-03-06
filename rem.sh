42969433b8cd6b3c7bfe5450fb1d2d7154dee823
らずるしぇーにゃ <razrusheniya@outlook.com>
2026-03-06 17:42:59 +0900

    Update command rem.sh

--- rem.sh
+++ rem.sh
@@ -1,29 +1,16 @@
-run() {
-    cat $1 | rem > main.asm
-    shift
-
-    nasm -f elf64 -g -F dwarf -o main.o main.asm
-    gcc -no-pie -O3 -rdynamic -o main main.o $(pkg-config --cflags --libs gtk+-3.0)
-    ./main "$@"
-}
-
-install() {
+if [ $1 = "--update" ]; then
+    git stash
+    git pull
+    cargo install --path .
+elif [ $1 = "--install" ]; then
     apt install nasm gcc libgtk-3-dev pkg-config curl
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
     . "$HOME/.cargo/env"
     cargo install --path .
-}
-
-update() {
-    git stash
-    git pull
-    cargo install --path .
-}
-
-if [ $1 = "--update" ]; then
-    update
-elif [ $1 = "--install" ]; then
-    install
 else
-    run "$@"
+    cat $1 | rem > main.asm 
+    shift
+    nasm -f elf64 -g -F dwarf -o main.o main.asm
+    gcc -no-pie -O3 -rdynamic -o main main.o $(pkg-config --cflags --libs gtk+-3.0)
+    ./main "$@"
 fi

fdf652277ab34b2c622f08306153ef7875781e31
らずるしぇーにゃ <razrusheniya@outlook.com>
2026-03-08 10:40:03 +0900

    Fix error in emit.rs

--- src/parse.rs
+++ src/parse.rs
@@ -9,7 +9,8 @@ impl Define {
             if let Some(func) = line.strip_prefix("fn ") {
                 let (head, body) = ok!(func.split_once(")"))?;
                 let (name, args) = ok!(head.split_once("("))?;
-                let args = tokenize(args, ",")?.iter().map(Name::new)
+                let args = tokenize(args, ",")?
+                    .iter().map(String::as_str).map(Name::new)
                     .collect::<Result<IndexSet<Name>, String>>()?;
                 let body = Expr::parse(body)?;
                 result.push(Define(Name::new(name)?, args, body));
